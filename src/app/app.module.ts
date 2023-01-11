b9936f7940b074a7edf7b8d2545ff9dac7aea754
Krystof Kratky <krystof.kratky2003@gmail.com>
2023-01-11 02:51:08 +0100

    fix: functional angular form in habits management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HabitsManagementModalComponent } from './habits-management-modal/habits-management-modal.component';
-import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,7 @@ import { AboutComponent } from './about/about.component';
     HomeComponent,
     AboutComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/habits-management-modal/habits-management-modal.component.ts
+++ src/app/habits-management-modal/habits-management-modal.component.ts
@@ -1,63 +1,11 @@
 import { Component, Input, inject } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-habits-management-modal',
   providers: [CookieService],
-  template: `
-    <div *ngIf="isOpen">
-      <div class="modal-overlay" (click)="onClose()"></div>
-      <div class="modal">
-        <button type="button" class="close" (click)="onClose()">
-          <span class="icon">x</span>
-        </button>
-
-        <h1 class="title">Manage habits</h1>
-
-        <section>
-          <table>
-            <thead>
-              <tr>
-                <th>Habit</th>
-                <th *ngFor="let day of days">{{ day }}</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              <tr *ngFor="let habit of habits">
-                <td class="habit">
-                  {{ habit }}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="trash-icon"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                  </svg>
-                </td>
-                <td *ngFor="let day of days">
-                  <input type="checkbox" />
-                </td>
-              </tr>
-              <tr class="new" (click)="handleAddHabit()">
-                <td>
-                  <span class="icon">+</span>
-                  Add habit
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-      </div>
-    </div>
-  `,
+  templateUrl: './habits-management-modal.component.html',
   styleUrls: ['./habits-management-modal.component.scss'],
 })
 export class HabitsManagementModalComponent {
@@ -66,29 +14,42 @@ export class HabitsManagementModalComponent {
   @Input()
   isOpen: boolean = false;
 
-  habits: string[] = [];
   days = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
 
   onClose = () => {
     this.isOpen = false;
   };
 
-  handleAddHabit = () => {
-    this.habits.push('New habit');
-    this.cookieService.set('habits', JSON.stringify(this.habits));
-  };
+  myForm: FormGroup;
 
-  getHabits = (): string[] => {
-    return JSON.parse(this.cookieService.get('habits') || '[]') || [];
-  };
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      habits: this.fb.array([]),
+    });
+  }
 
-  constructor() {
-    const tmpHabits = this.getHabits();
+  get habitForms() {
+    return this.myForm.get('habits') as FormArray;
+  }
 
-    if (tmpHabits.length > 0) {
-      this.habits = tmpHabits;
-    } else {
-      this.habits = ['8hrs of sleep', 'Meditation', 'Running', 'Reading'];
-    }
+  addHabit() {
+    const habit = this.fb.group({
+      title: [],
+      days: this.fb.array([true, true, true, true, true, true, true]),
+    });
+
+    this.habitForms.push(habit);
+  }
+
+  deleteHabit(i: number) {
+    this.habitForms.removeAt(i);
+  }
+
+  updateDay(i: number, ii: number) {
+    this.habitForms.at(i).patchValue({
+      days: this.habitForms.value[i].days.map((day: boolean, index: number) =>
+        index === ii ? !day : day
+      ),
+    });
   }
 }
