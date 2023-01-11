359c1103b7eaa5b1e630c4bc742030bea30a82fe
Krystof Kratky <krystof.kratky2003@gmail.com>
2023-01-11 03:23:18 +0100

    fix: remove cookies & functional local storage implementation

--- src/app/habits-management-modal/habits-management-modal.component.ts
+++ src/app/habits-management-modal/habits-management-modal.component.ts
@@ -1,16 +1,12 @@
-import { Component, Input, inject } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-habits-management-modal',
-  providers: [CookieService],
   templateUrl: './habits-management-modal.component.html',
   styleUrls: ['./habits-management-modal.component.scss'],
 })
 export class HabitsManagementModalComponent {
-  cookieService = inject(CookieService);
-
   @Input()
   isOpen: boolean = false;
 
@@ -25,6 +21,24 @@ export class HabitsManagementModalComponent {
   constructor(private fb: FormBuilder) {
     this.myForm = this.fb.group({
       habits: this.fb.array([]),
+    });
+
+    const habits = JSON.parse(localStorage.getItem('habits') || '[]');
+
+    habits.forEach((habit: any) => {
+      const habitForm = this.fb.group({
+        title: habit.title || 'New habit',
+        days: this.fb.array(
+          habit.days || [true, true, true, true, true, true, true]
+        ),
+      });
+
+      this.habitForms.push(habitForm);
+    });
+
+    // on form update save data to local storage
+    this.myForm.valueChanges.subscribe((value) => {
+      localStorage.setItem('habits', JSON.stringify(value.habits));
     });
   }
 
