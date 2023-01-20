a278ca1b87f5c22698ed9451f45c8aa217532fe8
Krystof Kratky <krystof.kratky2003@gmail.com>
2023-01-20 13:26:40 +0100

    folder structure update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule, Routes } from '@angular/router'
-import { HomeComponent } from './home/home.component'
-import { AboutComponent } from './about/about.component'
+import { HomeComponent } from './pages/home/home.component'
+import { AboutComponent } from './pages/about/about.component'
 
 const routes: Routes = [
   {
